1fb9bec3e2f44ae2d36fe1c45cfb802a2b4f6337
Joey Wang <joey840404@gmail.com>
2021-11-05 00:46:44 -0700

    login and logout showing welcome message

--- src/notecatcher/client/app.cljs
+++ src/notecatcher/client/app.cljs
@@ -1,18 +1,39 @@
 (ns notecatcher.client.app
   (:require
+    [citrus.core :as citrus]
     [goog.dom :as gdom]
+    [notecatcher.client.hooks :as hooks]
+    [notecatcher.firebase :as firebase]
+    [notecatcher.reconciler :as reconciler]
     [rum.core :as rum]))
 
 
-(rum/defc Test < rum/static
-          []
-          [:div "Hello World!"])
+(rum/defc Test  < rum/static
+          [r]
+          (let [user (first (hooks/use-sub r [:app :user]))]
+            [:div
+             [:h1 "Notecatcher"]
+             (when user
+               [:h2 (str "Welcome! " (:display-name user))])
+             (when-not user
+               [:button
+                {:on-click #(firebase/sign-in-with-google)}
+                "Login"])
+             (when user
+               [:button
+                {:on-click #(firebase/sign-out)}
+                "Logout"])
+             [:button
+              {:on-click #(citrus/dispatch! r :app :test "hi!")}
+              "test dispatch"]]))
 
 
 (defn main
   []
+  (reconciler/initilize-reconciler)
+  (firebase/init)
   (rum/mount
-    (Test)
+    (Test @reconciler/r)
     (gdom/getElement "main-app")))
 
 
