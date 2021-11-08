f51549cc5b19c007f3f99ba467aa3772e7499f27
Joey Wang <joey840404@gmail.com>
2021-11-07 16:14:47 -0800

    handle-new-users function that add a doc to firestore

--- src/notecatcher/server/functions.cljs
+++ src/notecatcher/server/functions.cljs
@@ -1,7 +1,27 @@
 (ns notecatcher.server.functions
   (:require
+    ["firebase-admin" :default firebase-admin]
     ["firebase-functions" :as functions]
+    [cljs-bean.core :as cljs-bean]
     [notecatcher.server.index-page :as index-page]))
+
+
+(defonce firebase-app (atom nil))
+
+
+(defn init-app
+  []
+  (when-not @firebase-app
+    (reset! firebase-app
+            (.initializeApp firebase-admin))))
+
+
+(defn db
+  []
+  (.firestore firebase-admin))
+
+
+(init-app)
 
 
 (def handle-request-fn
@@ -11,6 +31,19 @@
                 (.send res "Hello World! I'm notecatcher."))))
 
 
+(def new-user-handler
+  (.onCreate (.. functions -auth user)
+             (fn [user-js]
+               (let [user (cljs-bean/->clj user-js)]
+                 (.set
+                   (.doc (db) (str "/user/" (:uid user)))
+                   #js {:display_name (:displayName user)
+                        :email (:email user)
+                        :uid (:uid user)
+                        :photo_url (:photoURL user)})))))
+
+
 (def exports
   #js {:handleRequest handle-request-fn
-       :index index-page/handler})
+       :index index-page/handler
+       :newUser new-user-handler})
